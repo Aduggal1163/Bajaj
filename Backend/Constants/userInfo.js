c76a89bf9f025343fb1b969f62f5e6f2193f71cd
Aduggal1163 <abhiduggal1163@gmail.com>
2025-07-29 20:45:52 +0530

    FIx Minor underscore issue to look more clean

--- Backend/Constants/userInfo.js
+++ Backend/Constants/userInfo.js
@@ -1,5 +1,5 @@
 export const USER_INFO = {
-  full_name: "Abhishek Duggal",
+  full_name: "Abhishek_Duggal",
   dob: "06082004",
   email: "abhishek1163.be22@chitkara.edu.in",
   roll_number: "221099163",
